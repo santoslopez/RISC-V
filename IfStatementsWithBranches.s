768e245d90b44c5406aff019c361d26975885e1a
Santos López <santos.lopez@galileo.edu>
2019-06-02 22:39:44 -0600

    If statements can be compiled using branches

---
+++ IfStatementsWithBranches.s
@@ -0,0 +1,14 @@
+#	MIT 6.004 L03: Compiling Code, Procedures, and Stacks
+
+
+#	example c code
+
+# 	if (expr) {
+#		if-body
+# 	}
+
+
+#	RISCV Assembly
+	beqz xN, endif
+#	(compile if-body)
+endif:
